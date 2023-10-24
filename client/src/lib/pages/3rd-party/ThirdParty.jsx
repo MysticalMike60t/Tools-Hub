d9c0dfa0c34e0b7793da2b6ece7240224f91029f
Mysticmike60t <caden.finkelstein@cttech.org>
2023-10-24 18:35:35 -0400

    Update ThirdParty.jsx

--- client/src/lib/pages/3rd-party/ThirdParty.jsx
+++ client/src/lib/pages/3rd-party/ThirdParty.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 const ThirdParty = () => {
+  const downloadUrl =
+    "https://api.cadenmf.com/downloads/tools/x64tools_10-24-2023.rar";
+  const fileName = "x64tools_10-24-2023.rar";
+
   return (
     <div className="page third-party">
       <h1>3rd Party Tools</h1>
       <p className="text">
-        <a
-          href="https://api.cadenmf.com/downloads/tools/x64tools_10-24-2023.rar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nirsoft Tools
+        <a href={downloadUrl} download={fileName}>
+          Download Nirsoft Tools
         </a>
       </p>
     </div>
