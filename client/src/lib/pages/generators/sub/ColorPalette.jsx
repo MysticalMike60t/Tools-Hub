9a4fcbbc5b10a8587ffcc211e1e6ea1364415b69
Mysticmike60t <caden.finkelstein@cttech.org>
2023-10-24 18:17:42 -0400

    Update ColorPalette.jsx

--- client/src/lib/pages/generators/sub/ColorPalette.jsx
+++ client/src/lib/pages/generators/sub/ColorPalette.jsx
@@ -29,6 +29,14 @@ const ColorPalette = () => {
     document.body.removeChild(el);
   }
 
+  function getContrastColor(hexColor) {
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness >= 128 ? '#000' : '#fff';
+  }
+
   return (
     <div className='page one color-palette'>
       <h1>Color Palette Generator</h1>
@@ -37,7 +45,7 @@ const ColorPalette = () => {
           <div
             key={index}
             className="color-box"
-            style={{ backgroundColor: color }}
+            style={{ backgroundColor: color, color: getContrastColor(color) }}
             onClick={() => copyToClipboard(color)}
           >
             {color}
