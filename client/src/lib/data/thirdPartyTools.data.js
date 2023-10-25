ed59a190f2c45132693744016ea7f852b68e33a6
Mysticmike60t <caden.finkelstein@cttech.org>
2023-10-24 20:23:17 -0400

    sd

---
+++ client/src/lib/data/thirdPartyTools.data.js
@@ -0,0 +1,50 @@
+const thirdPartyTools = [
+  {
+    category: "Windows Tools",
+    sections: [
+      {
+        sectionName: "Nirsoft",
+        tools: [
+          {
+            name: "Nirsoft Tools",
+            link: "https://api.cadenmf.com/downloads/tools/x64tools_10-24-2023.rar",
+          },
+        ],
+      },
+      {
+        sectionName: "Resource Hacker",
+        tools: [
+          {
+            name: "Resource Hacker",
+            link: "https://angusj.com/resourcehacker/reshacker_setup.exe",
+          },
+        ],
+      },
+      {
+        sectionName: "XN Resource Editor",
+        tools: [
+          {
+            name: "XN Resource Editor Installer",
+            link: "https://github.com/stefansundin/xn_resource_editor/releases/download/v3.0.0.1/xn_resourceeditor_setup.exe",
+          },
+          {
+            name: "XN Resource Editor Portable",
+            link: "https://github.com/stefansundin/xn_resource_editor/releases/download/v3.0.0.1/XNResourceEditor.exe",
+          },
+        ],
+      },
+      {
+        sectionName: "PS Explorer",
+        tools: [
+          {
+            name: "PEX Setup",
+            link: "http://www.heaventools.com/download/pexsetup.exe?r1=pexCom",
+          },
+        ],
+      },
+    ],
+  },
+  // Add more categories and sections as needed
+];
+
+export default thirdPartyTools;
--- client/src/lib/pages/3rd-party/ThirdParty.jsx
+++ client/src/lib/pages/3rd-party/ThirdParty.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { thirdPartyTools } from "../../data";
 
 const ThirdParty = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -8,56 +9,8 @@ const ThirdParty = () => {
   };
 
   // Sample data with preset categories and sections
-  const thirdPartyData = [
-    {
-      category: "Windows Tools",
-      sections: [
-        {
-          sectionName: "Nirsoft",
-          tools: [
-            {
-              name: "Nirsoft Tools",
-              link: "https://api.cadenmf.com/downloads/tools/x64tools_10-24-2023.rar",
-            },
-          ],
-        },
-        {
-          sectionName: "Resource Hacker",
-          tools: [
-            {
-              name: "Resource Hacker",
-              link: "https://angusj.com/resourcehacker/reshacker_setup.exe",
-            },
-          ],
-        },
-        {
-          sectionName: "XN Resource Editor",
-          tools: [
-            {
-              name: "XN Resource Editor Installer",
-              link: "https://github.com/stefansundin/xn_resource_editor/releases/download/v3.0.0.1/xn_resourceeditor_setup.exe",
-            },
-            {
-              name: "XN Resource Editor Portable",
-              link: "https://github.com/stefansundin/xn_resource_editor/releases/download/v3.0.0.1/XNResourceEditor.exe",
-            },
-          ],
-        },
-        {
-          sectionName: "PS Explorer",
-          tools: [
-            {
-              name: "PEX Setup",
-              link: "http://www.heaventools.com/download/pexsetup.exe?r1=pexCom",
-            },
-          ],
-        },
-      ],
-    },
-    // Add more categories and sections as needed
-  ];
 
-  const filteredTools = thirdPartyData.map((category) => ({
+  const filteredTools = thirdPartyTools.map((category) => ({
     ...category,
     sections: category.sections.filter((section) =>
       section.tools.some((tool) =>
