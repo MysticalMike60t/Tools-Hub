ad862501ec2902bd9e4a6c9596c5ce9e631834a5
Caden F <caden.finkelstein@cttech.org>
2023-10-26 09:52:16 -0400

    sd

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string processName = "node"; // Change this to the name of your Node.js process
+
+        if (IsNodeProcessRunning(processName))
+        {
+            Console.WriteLine($"Node.js process ({processName}) is running.");
+        }
+        else
+        {
+            Console.WriteLine($"Node.js process ({processName}) is not running.");
+        }
+    }
+
+    static bool IsNodeProcessRunning(string processName)
+    {
+        Process[] processes = Process.GetProcessesByName(processName);
+
+        return processes.Length > 0;
+    }
+}
