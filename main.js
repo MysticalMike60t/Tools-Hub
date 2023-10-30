547e0ec6542aad912ad3c0fe9025f13aefead5f9
Caden F <caden.finkelstein@cttech.org>
2023-10-30 13:27:53 -0400

    Trying to fix update, lol

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ const windowAcrylicDisableOnBlur = false;
 const windowTransparent = true;
 const windowResizable = false;
 const windowTitleBarStyle = "hidden";
-// const windowTitleBarOverlayColor = "#2f3241";
 const windowTitleBarOverlayColor = "rgba(47, 50, 65, 0)";
 const windowTitleBarOverlaySymbolColor = "#74b1be";
 const windowsIconPath = path.join(__dirname, "lib/images/icons/toolbox.png");
@@ -69,7 +68,89 @@ function createWindow() {
   mainWindow.loadFile(indexHTMLPath);
 }
 
+function checkForUpdates() {
+  // Check for updates without automatically downloading
+  autoUpdater.checkForUpdates();
+
+  // Get the current app version
+  const currentVersion = app.getVersion();
+
+  autoUpdater.on("update-available", (info) => {
+    console.log("Update available:", info.version);
+
+    // Compare the GitHub version with the current app version
+    if (compareVersions(info.version, currentVersion) > 0) {
+      const message = `A new version of the app is available.\nCurrent Version: ${currentVersion}\nLatest Version: ${info.version}`;
+      console.log(message); // Log to the Windows console
+
+      const options = {
+        type: "info",
+        title: "Update Available",
+        message,
+        buttons: ["Yes", "No"],
+      };
+
+      dialog.showMessageBox(options, (buttonIndex) => {
+        if (buttonIndex === 0) {
+          // User clicked "Yes," so quit the app and install the update
+          autoUpdater.downloadUpdate();
+        }
+      });
+    }
+  });
+}
+
+function showMsg() {
+  const currentVersion = app.getVersion();
+
+  // Fetch the latest version from GitHub using the GitHub API
+  // fetch(
+  //   `https://api.github.com/repos/${githubOwner}/${githubRepo}/releases/latest`
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     const latestVersion = data.tag_name;
+  //     const message = `Current Version: ${currentVersion}\nLatest Version: ${latestVersion}`;
+
+  //     dialog.showMessageBox({
+  //       type: "info",
+  //       title: "App Version Info",
+  //       message,
+  //       buttons: ["OK"],
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching latest version:", error);
+  //     dialog.showMessageBox({
+  //       type: "error",
+  //       title: "Error",
+  //       message: "Failed to fetch the latest version from GitHub",
+  //       buttons: ["OK"],
+  //     });
+  //   });
+}
+
+function compareVersions(a, b) {
+  // Implement your version comparison logic here.
+  // This is a simple example; you can use a more robust library for this.
+  const versionA = a.split(".").map(Number);
+  const versionB = b.split(".").map(Number);
+
+  for (let i = 0; i < versionA.length; i++) {
+    if (versionA[i] < versionB[i]) return -1;
+    if (versionA[i] > versionB[i]) return 1;
+  }
+
+  return 0;
+}
+
 app.on("ready", () => {
+  // Redirect the console output to the Windows console
+  console.log = function(message) {
+    const cp = require("child_process");
+    cp.exec(`echo ${message}`);
+  };
+
   // Configure autoUpdater
   autoUpdater.autoDownload = autoUpdateDownload;
   autoUpdater.allowPrerelease = autoUpdateAllowPrerelease;
@@ -87,31 +168,34 @@ app.on("ready", () => {
   });
 
   // Check for updates and notify the user
-  autoUpdater.checkForUpdatesAndNotify();
+  checkForUpdates();
+
+  showMsg();
 
   createWindow();
 });
 
-autoUpdater.on("update-available", () => {
-  console.log("Update available");
+autoUpdater.on("update-available", (info) => {
+  console.log("Update available:", info.version);
   // Notify the user or add more logic here
-});
 
-autoUpdater.on("update-downloaded", () => {
   const options = {
     type: "info",
     title: "Update Available",
-    message:
-      "A new version of the app is available. Do you want to install it now?",
+    message: `A new version (${info.version}) of the app is available. Do you want to install it now?`,
     buttons: ["Yes", "No"],
   };
 
   dialog.showMessageBox(options, (buttonIndex) => {
     if (buttonIndex === 0) {
       // User clicked "Yes," so quit the app and install the update
-      autoUpdater.quitAndInstall();
+      autoUpdater.downloadUpdate();
     }
   });
+});
+
+autoUpdater.on("update-downloaded", () => {
+  // This event is no longer used for the prompt, as the update will be downloaded when the user clicks "Yes."
 });
 
 autoUpdater.on("error", (error) => {
