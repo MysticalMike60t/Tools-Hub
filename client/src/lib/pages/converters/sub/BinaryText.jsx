eb5cb332b4e43737772d719c62fc7daf0ace4af6
Caden F <caden.finkelstein@cttech.org>
2023-11-01 13:03:26 -0400

    Added binary to text converter

--- client/src/lib/pages/converters/sub/BinaryText.jsx
+++ client/src/lib/pages/converters/sub/BinaryText.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const BinaryText = () => {
-  return (
-    <div>BinaryText</div>
-  )
-}
+  const [binaryInput, setBinaryInput] = useState("");
+  const [textOutput, setTextOutput] = useState("");
 
-export default BinaryText
+  const convertBinaryToText = () => {
+    try {
+      const binaryArray = binaryInput.split(" ");
+      const text = binaryArray
+        .map((binary) => String.fromCharCode(parseInt(binary, 2)))
+        .join("");
+      setTextOutput(text);
+    } catch (error) {
+      setTextOutput("Invalid binary input");
+    }
+  };
+  return (
+    <div className="page one binary-text">
+      <h1>Binary to Text Converter</h1>
+      <div className="wrapper">
+        <label>Enter Binary:</label>
+        <textarea
+          type="text"
+          value={binaryInput}
+          onChange={(e) => setBinaryInput(e.target.value)}
+        />
+      </div>
+      <div className="wrapper">
+        <button onClick={convertBinaryToText}>Convert</button>
+      </div>
+      <div className="wrapper">
+        <label>Text Output:</label>
+        <div className="output">{textOutput}</div>
+      </div>
+    </div>
+  );
+};
+
+export default BinaryText;
